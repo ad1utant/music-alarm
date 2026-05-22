28f70d432fb2997cdcffc1be229e136ffb6a34a9
adiutant <mikolaj@marasek.eu>
2026-05-22 02:45:23 +0200

    all options work properly

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from ast import Index
+from copy import deepcopy
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base, sessionmaker
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -57,16 +59,50 @@ with Session() as session:
         scheduler.add_job(handle_alarm, trigger='cron', hour=int(h), minute=int(m), day_of_week = flags, id = str(alarm.id))
 scheduler.print_jobs()
 
+def display():
+    global scheduler
+    for job in scheduler.get_jobs():
+        print(f'ID: {job.id}', end='\t')
+        print(job.next_run_time.strftime('%H:%M'), end='\t')
+        days = str(job.trigger.fields[4])
+        for d in days:
+            if d.isdigit():
+                print(int(d) + 1, end='')
+            else:
+                print(d, end='')
+        print()
+
+
 while True:
     print('s - to set \nr - to remove\nd - to display')
     option = input()
+    if option == 'd':
+        display()
+        input()
     if option == 's':
         while True:
-            command = raw_command_translation(input())
-            if not command: break
-            with Session() as session:
-                new_alarm = Alarm(hour=command['hour'], minute = command['minute'], flags = command['raw_flags'])
-                session.add(new_alarm)
-                session.commit()
-                scheduler.add_job(handle_alarm, trigger='cron', hour=command['hour'], minute=command['minute'], day_of_week=command['flags'], id=str(new_alarm.id))
-            scheduler.print_jobs()
+            try:
+                command = raw_command_translation(input())
+                if not command: break
+                with Session() as session:
+                    new_alarm = Alarm(hour=command['hour'], minute = command['minute'], flags = command['raw_flags'])
+                    session.add(new_alarm)
+                    session.commit()
+                    scheduler.add_job(handle_alarm, trigger='cron', hour=command['hour'], minute=command['minute'], day_of_week=command['flags'], id=str(new_alarm.id))
+            except Exception:
+                print('command you entered is incorrect')
+    if option == 'r':
+        display()
+        while True:
+            choice = input()
+            if choice == '': break
+            try:
+                scheduler.remove_job(choice)
+                with Session() as session:
+                    alarm_to_delete = session.query(Alarm).filter(Alarm.id == int(choice)).first()
+                    if alarm_to_delete:
+                        session.delete(alarm_to_delete)
+                        session.commit()
+                print('alarm removed successfully')
+            except Exception:
+                print('id you entered is incorrect')
